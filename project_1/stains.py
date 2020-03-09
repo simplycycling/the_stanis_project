a9876fe4f5530b7b4843f7db210cc21ef4433c1a
rsherman <Roger.Sherman@quantium.com.au>
2020-03-09 21:08:48 +1100

    Updated variable names so that Stanis is no longer inside of Sharpy

--- project_1/stains.py
+++ project_1/stains.py
@@ -8,29 +8,31 @@ from collections import Counter
 # So keys are usernames, values are names. Note that commas inside the name get replaced by a single space.
 # If it’s a service account, strip off svc_, replace any underscores with spaces.
 def get_users():
-    shawsie = {}
-    stanis = open("passwd", "r")
-    sharpy = stanis.readlines()
+    usernames = {}
+    handle = open("project_1/passwd", "r")
+    handle_lines = handle.readlines()
 
-    for lines in sharpy:
-        polina = lines.replace("svc_", "")
-        yury = polina.replace("_", " ")
-        marcelo = yury.strip().split(":")
-        shawsie[marcelo[0]] = os.path.basename(marcelo[6])
+    for lines in handle_lines:
+        remove_svc = lines.replace("svc_", "")
+        remove_underscores = remove_svc.replace("_", " ")
+        cleanup = remove_underscores.strip().split(":")
+        user = cleanup[0]
+        shells = os.path.basename(cleanup[6])
+        usernames[user] = shells
 
-    stanis.close()
-    return shawsie
+    handle.close()
+    return usernames
 
 
 # return how many users use each shell. Write your stats to shell.stats
 def get_shell_stats():
-    fukkin_wade = get_users()
-    angus_who = open("shell.stats", "w")
-    albie = Counter(fukkin_wade.values())
-    for key, c in albie.most_common():
-        #angus_who.write("{}: {}".format(key, c))
+    all_users = get_users()
+    handle = open("shell.stats", "w")
+    get_count = Counter(all_users.values())
+    for key, c in get_count.most_common():
+        print("{}: {}".format(key, c), file=handle)
         print("{}: {}".format(key, c))
-    angus_who.close()
+    handle.close()
 
 
 if __name__ == '__main__':
